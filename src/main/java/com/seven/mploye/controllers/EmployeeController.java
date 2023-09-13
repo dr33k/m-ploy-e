006d157d39917e576e37aedec2293dbd3c431dfe
dr33k <dr33k@users.noreply.github.com>
2023-09-13 08:37:27 +0100

    Added create functionality

--- src/main/java/com/seven/mploye/controllers/EmployeeController.java
+++ src/main/java/com/seven/mploye/controllers/EmployeeController.java
@@ -1,19 +1,39 @@
 package com.seven.mploye.controllers;
 
-import org.springframework.ui.Model;
+import com.seven.mploye.Employee;
+import com.seven.mploye.EmployeeRepository;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
-@RestController
-@RequestMapping("/employee")
+@Controller
+@RequestMapping("/employees/")
 public class EmployeeController {
-    @GetMapping
-    public String getEmployee(Model model){
-        return "Emelie";
+    private EmployeeRepository employeeRepository;
+    public EmployeeController(EmployeeRepository employeeRepository){
+        this.employeeRepository = employeeRepository;
     }
-    @GetMapping("/w")
-    public String getEmployee2(Model model){
-        return "new Employee of the Ivantoe tower";
+    @GetMapping({"list",""})
+    public ModelAndView getEmployee2(){
+        ModelAndView mav = new ModelAndView("parent");
+        mav.addObject("fragment", "list_employees");
+        mav.addObject("employees", employeeRepository.findAll());
+        return mav;
+    }
+    @GetMapping("create")
+    public ModelAndView createEmployeeView(){
+        ModelAndView mav = new ModelAndView("parent");
+        mav.addObject("fragment", "create_employees");
+        mav.addObject("employee", new Employee());
+        return mav;
+    }
+
+    @PostMapping("create")
+    public String createEmployee(@ModelAttribute Employee employee){
+        employeeRepository.save(employee);
+        return "redirect:/employees/list";
     }
 }
